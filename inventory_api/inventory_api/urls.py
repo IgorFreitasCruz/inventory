4494b363951b2f917a95d26f6350e3ee9d2c4d88
Igor de Freitas Cruz <igor.freitas.cruz@icloud.com>
2023-01-24 08:57:45 -0300

    Added trailing slash to urls.py urlpatterns

--- inventory_api/inventory_api/urls.py
+++ inventory_api/inventory_api/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("user", include("user_control.urls")),
-    path("app", include("app_control.urls")),
+    path("user/", include("user_control.urls")),
+    path("app/", include("app_control.urls")),
 ]
